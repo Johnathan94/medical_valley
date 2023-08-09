02d690e3d5195d95dba1e898c4ea371ce9de4d51
Mohamed imbaby <mohamedimbaby999@gmail.com>
2023-08-09 15:02:19 +0300

    finish user bugs

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -10,7 +10,7 @@ import 'package:medical_valley/core/dialogs/loading_dialog.dart';
 import 'package:medical_valley/core/shared_pref/shared_pref.dart';
 import 'package:medical_valley/core/strings/images.dart';
 import 'package:medical_valley/features/auth/login/presentation/screens/login_screen.dart';
-import 'package:medical_valley/features/auth/register/presentation/registeration_screen.dart';
+import 'package:medical_valley/features/home/widgets/home_base_stateful_widget.dart';
 import 'package:medical_valley/features/splash/presentation/screens/no_location_service_screen.dart';
 import 'package:medical_valley/features/welcome_page/splash_bloc.dart';
 import 'package:network_logger/network_logger.dart';
@@ -48,8 +48,8 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   goToHomeScreen(BuildContext context) {
-    Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const RegistrationScreen()));
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => const HomeBaseStatefulWidget()));
   }
 
   @override
