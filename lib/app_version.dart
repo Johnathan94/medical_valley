00a47b5c2b69cb4e35ecd11d329fe5055e2c21a3
Mohamed imbaby <mohamedimbaby999@gmail.com>
2023-08-26 23:16:47 +0300

    raise a version to 2.0.1 / update app settings

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,5 +1,5 @@
 class AppSettings {
-  static String appVersion = "2.0.0";
-  static String description = "added resend otp "
-      "139";
+  static String appVersion = "2.0.1";
+  static String description =
+      "solve offers screen and make it reachable from requests screen";
 }
