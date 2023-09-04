c1a5d1df5db4fced7144cf8d25511e798c230552
elkholy <boodykh@gmail.com>
2023-09-05 00:59:01 +0300

    increased version -> added handling notifications actions

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,4 +1,4 @@
 class AppSettings {
-  static String appVersion = "2.0.8";
-  static String description = "added notifications integration";
+  static String appVersion = "2.0.9";
+  static String description = "added handling notifications actions";
 }
